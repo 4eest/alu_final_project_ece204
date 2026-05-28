1e5592e97c2db586b4dd104417c0453719d4507f
thaynelowe05-netizen <thaynelowe05@gmail.com>
2026-05-27 19:33:07 -0700

    ready for forrest

--- Opcode.sv
+++ Opcode.sv
@@ -2,12 +2,7 @@ module opcode(
 
 	input logic [2:0] sw,
 		
-	output logic [7:0] enable_or,
-	output logic [7:0] enable_xor,
-	output logic [7:0] enable_and,
-	output logic [7:0] enable_nand,
-	output logic [7:0] enable_add,
-	output logic [7:0] enable_subtract
+	output logic [7:0] opcode_decode
 
 
 );
@@ -16,36 +11,22 @@ logic enable = 1;
 
 always_comb begin
 	
-	enable_or = 0;
-   enable_xor = 0;
-   enable_and = 0;
-	enable_nand = 0;
-   enable_add = 0;
-   enable_subtract = 0;
-	enable_outA = 0;
-	enable_outB = 0;
+	opcode_decode = 0;
 	
 	case(enable)
 
-		3'b000: enable_add      = 8’b10000000;
-      3'b001: enable_subtract = 8’b01000000;
-      3'b010: enable_and      = 8’b00100000;
-      3'b011: enable_xor      = 8’b00010000;
-		3'b100: enable_or       = 8’b00001000;
-		3'b101: enable_outA     = 8’b00000100;
-      3'b110: enable_outB     = 8’b00000010;
-      3'b111: enable_nand     = 8’b00000001;
+		3'b000: opcode_decode     = 8'b10000000;
+      3'b001: opcode_decode = 8'b01000000;
+      3'b010: opcode_decode      = 8'b00100000;
+      3'b011: opcode_decode      = 8'b00010000;
+		3'b100: opcode_decode       = 8'b00001000;
+		3'b101: opcode_decode     = 8'b00000100;
+      3'b110: opcode_decode     = 8'b00000010;
+      3'b111: opcode_decode     = 8'b00000001;
 
 		
 		default: begin
-			enable_or       = 0;
-         enable_xor      = 0;
-         enable_and      = 0;
-         enable_nand     = 0;
-         enable_add      = 0;
-         enable_subtract = 0;
-			enable_outA     = 0;
-			enable_outB     = 0;
+			opcode_decode      = 0;
 
       end
 	endcase
--- Top-Level-Display.sv
+++ Top-Level-Display.sv
@@ -1,8 +1,15 @@
 module Top_Level(
+
+	input logic save,
+	input logic [7:0] inputA,
+	input logic [7:0] inputB,
 	input logic reset_n,
-	input logic [7:0] sw,
+	input logic [7:0] data_in,
+	input logic enable,
 	output logic [6:0] seg0,
-	output logic [6:0] seg1
+	output logic [6:0] seg1,
+	output logic [6:0] seg2,
+	output logic [7:0] LED_out
 
 );
 
@@ -11,47 +18,29 @@ logic [7:0] inputA = 0;
 logic [7:0] inputB = 0;
 logic [2:0] opcode = 0;
 logic [7:0] result = 0;
-logic [7:0] result_or = 0;
-logic [7:0] result_xor = 0;
-logic [7:0] result_and = 0;
-logic [7:0] result_nand = 0;
-logic [7:0] result_add = 0;
-logic [7:0] result_subtract = 0;
 logic overflow = 0;
 //more wires needed for parser -> sevensegdecoder
 logic [3:0] ones_place;
 logic [3:0] tens_place;
+logic [3:0] hundreds_place;
+logic [7:0] opcode_decode;
 
 
-// count A
-
-// count B
 
 Opcode decoder(
 
 	.sw(sw),
-	.enable_or(enable_or),
-	.enable_xor(enable_xor),
-	.enable_and(enable_and),
-	.enable_nand(enable_nand),
-	.enable_add(enable_add),
-	.enable_subtract(enable_subtract)
+	.opcode_decode(opcode_decode)
 );
 
 ALU Calculator(
-
+	.reset_n(reset_n),
 	.inputA(inputA),
 	.inputB(inputB),
 	.result(result),
-	.result_or(result_or),
-	.result_xor(result_xor),
-	.result_add(result_add),
-	.result_nand(result_nand),
-	.result_add(result_add),
-	.result_subtract(result_subtract),
-	.overflow(overflow)
-
-);
+	.overflow(overflow),
+	.opcode(opcode_decode)
+	);
 
 
 
@@ -74,7 +63,12 @@ SevenSegmentDecode Hex1(
 	.segments(seg1)
 );
 
+SevenSegmentDecode Hex2(
+	.digit(hundreds_place),
+	.segments(seg2)
+);
 
+assign LED_out = result;
 
 
 endmodule
